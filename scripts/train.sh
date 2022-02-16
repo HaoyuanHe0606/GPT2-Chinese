3509c3aa6b8cdcc97c8f2d67f497b04a87ab99a2
Haoyuan He <48301431+HaoyuanHe0606@users.noreply.github.com>
2022-02-16 13:51:28 -0500

    updated

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,10 +3,10 @@ python train.py \
   --tokenized_data_path data/tokenized/ \
   --tokenizer_path cache/vocab_small.txt \
   --raw_data_path data/train.json \
-  --epochs 20 \
-  --log_step 150 \
+  --epochs 30 \
+  --log_step 200 \
   --stride 512 \
   --output_dir model/ \
-  --device 1,2,3 \
+  --device 0,1,2,3 \
   --num_pieces 100 \
   --raw
